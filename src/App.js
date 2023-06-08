25df5e709c2d1226fc78eada85d3926189296e6b
Amartya Sen <amartyasen895@gmail.com>
2023-06-08 18:43:49 +0530

    updated index

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Statistics from "./components/Statistics";
 import InputField from "./components/InputField";
 import Data from "./Data";
 
-const SECONDS = 3; // 5 minutes
+const SECONDS = 10; // 5 minutes
 
 function formatTime(time) {
   const minutes = Math.floor(time / 60);
@@ -184,7 +184,7 @@ function App() {
             fontWeight: 600,
           }}
         >
-          Typing Pro
+         Touch Typing Pro
         </h1>
         <div>
           {status === "finished" && (
